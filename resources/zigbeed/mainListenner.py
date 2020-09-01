cc2ef193aef0e650f6af1b22aaef6de599160d70
Loïc <loic@jeedom.com>
2020-09-01 11:59:18 +0200

    typo

--- resources/zigbeed/mainListenner.py
+++ resources/zigbeed/mainListenner.py
@@ -18,34 +18,34 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 class MainListener:
-    """
-    Contains callbacks that zigpy will call whenever something happens.
-    Look for `listener_event` in the Zigpy source or just look at the logged warnings.
-    """
+	"""
+	Contains callbacks that zigpy will call whenever something happens.
+	Look for `listener_event` in the Zigpy source or just look at the logged warnings.
+	"""
 
-    def __init__(self, application):
-        self.application = application
+	def __init__(self, application):
+		self.application = application
 
-    def device_joined(self, device):
-        print(f"Device joined: {device}")
+	def device_joined(self, device):
+		print("************************* Device joined: {device}")
 
-    def device_initialized(self, device, *, new=True):
-        """
-        Called at runtime after a device's information has been queried.
-        I also call it on startup to load existing devices from the DB.
-        """
-        LOGGER.info("Device is ready: new=%s, device=%s", new, device)
-        for ep_id, endpoint in device.endpoints.items():
-            # Ignore ZDO
-            if ep_id == 0:
-                continue
-            # You need to attach a listener to every cluster to receive events
-            for cluster in endpoint.in_clusters.values():
-                # The context listener passes its own object as the first argument
-                # to the callback
-                cluster.add_context_listener(self)
+	def device_initialized(self, device, *, new=True):
+		"""
+		Called at runtime after a device's information has been queried.
+		I also call it on startup to load existing devices from the DB.
+		"""
+		LOGGER.info("******************** Device is ready: new=%s, device=%s", new, device._ieee)
+		for ep_id, endpoint in device.endpoints.items():
+			# Ignore ZDO
+			if ep_id == 0:
+				continue
+			# You need to attach a listener to every cluster to receive events
+			for cluster in endpoint.in_clusters.values():
+				# The context listener passes its own object as the first argument
+				# to the callback
+				cluster.add_context_listener(self)
 
-    def attribute_updated(self, cluster, attribute_id, value):
-        # Each object is linked to its parent (i.e. app > device > endpoint > cluster)
-        device = cluster.endpoint.device
-        LOGGER.info("Received an attribute update %s=%s on cluster %s from device %s",attribute_id, value, cluster, device)
+	def attribute_updated(self, cluster, attribute_id, value):
+		# Each object is linked to its parent (i.e. app > device > endpoint > cluster)
+		device = cluster.endpoint.device
+		LOGGER.info("****************** Received an attribute update %s=%s on cluster %s from device %s",attribute_id, value, cluster.cluster_id, device._ieee)
