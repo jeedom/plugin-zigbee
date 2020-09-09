b6d27b6b4741361a9b70edb3326c7342ab01c5ec
Loïc <loic@jeedom.com>
2020-09-09 10:09:26 +0200

    add configuration option

--- core/class/zigbee.class.php
+++ core/class/zigbee.class.php
@@ -22,111 +22,105 @@ require_once __DIR__  . '/../../../../core/php/core.inc.php';
 class zigbee extends eqLogic {
   /*     * *************************Attributs****************************** */
   
-  /*
-  * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
-  * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
-  public static $_widgetPossibility = array();
-  */
-  
   /*     * ***********************Methode static*************************** */
   
   public static function dependancy_info() {
-		$return = array();
-		$return['progress_file'] = jeedom::getTmpFolder('zigbee') . '/dependance';
+    $return = array();
+    $return['progress_file'] = jeedom::getTmpFolder('zigbee') . '/dependance';
     $return['state'] = 'ok';
-		if (exec(system::getCmdSudo() . system::get('cmd_check') . '-E "python3\-serial|python3\-requests|python3\-pyudev" | wc -l') < 3) {
-			$return['state'] = 'nok';
-		}
+    if (exec(system::getCmdSudo() . system::get('cmd_check') . '-E "python3\-serial|python3\-requests|python3\-pyudev" | wc -l') < 3) {
+      $return['state'] = 'nok';
+    }
     if (exec(system::getCmdSudo() . 'pip list | grep -E "zigpy|bellows|zha-quirks|zigpy_znp|zigpy-xbee|zigpy-deconz|zigpy-zigate|zigpy-cc|tornado" | wc -l') < 9) {
-			$return['state'] = 'nok';
-		}
-		return $return;
-	}
-	
-	public static function dependancy_install() {
-		log::remove(__CLASS__ . '_update');
-		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('zigbee') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
-	}
+      $return['state'] = 'nok';
+    }
+    return $return;
+  }
+  
+  public static function dependancy_install() {
+    log::remove(__CLASS__ . '_update');
+    return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('zigbee') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
+  }
   
   public static function deamon_info() {
-		$return = array();
-		$return['log'] = 'zigbee';
-		$return['state'] = 'nok';
-		$pid_file = jeedom::getTmpFolder('zigbee') . '/deamon.pid';
-		if (file_exists($pid_file)) {
-			$pid = trim(file_get_contents($pid_file));
-			if (is_numeric($pid) && posix_getsid($pid)) {
-				$return['state'] = 'ok';
-			} else {
-				shell_exec(system::getCmdSudo() . 'rm -rf ' . $pid_file . ' 2>&1 > /dev/null;rm -rf ' . $pid_file . ' 2>&1 > /dev/null;');
-			}
-		}
-		$return['launchable'] = 'ok';
-		$port = config::byKey('port', 'zigbee');
-		if ($port != 'auto') {
-			$port = jeedom::getUsbMapping($port);
-			if (is_string($port)) {
-				if (@!file_exists($port)) {
-					$return['launchable'] = 'nok';
-					$return['launchable_message'] = __('Le port n\'est pas configuré', __FILE__);
-				}
-				exec(system::getCmdSudo() . 'chmod 777 ' . $port . ' > /dev/null 2>&1');
-			}
-		}
-		return $return;
-	}
+    $return = array();
+    $return['log'] = 'zigbee';
+    $return['state'] = 'nok';
+    $pid_file = jeedom::getTmpFolder('zigbee') . '/deamon.pid';
+    if (file_exists($pid_file)) {
+      $pid = trim(file_get_contents($pid_file));
+      if (is_numeric($pid) && posix_getsid($pid)) {
+        $return['state'] = 'ok';
+      } else {
+        shell_exec(system::getCmdSudo() . 'rm -rf ' . $pid_file . ' 2>&1 > /dev/null;rm -rf ' . $pid_file . ' 2>&1 > /dev/null;');
+      }
+    }
+    $return['launchable'] = 'ok';
+    $port = config::byKey('port', 'zigbee');
+    if ($port != 'auto') {
+      $port = jeedom::getUsbMapping($port);
+      if (is_string($port)) {
+        if (@!file_exists($port)) {
+          $return['launchable'] = 'nok';
+          $return['launchable_message'] = __('Le port n\'est pas configuré', __FILE__);
+        }
+        exec(system::getCmdSudo() . 'chmod 777 ' . $port . ' > /dev/null 2>&1');
+      }
+    }
+    return $return;
+  }
   
   public static function deamon_start() {
-		self::deamon_stop();
-		$deamon_info = self::deamon_info();
-		if ($deamon_info['launchable'] != 'ok') {
-			throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
-		}
-		$port = config::byKey('port', 'zigbee');
-		if ($port != 'auto') {
-			$port = jeedom::getUsbMapping($port);
-		}
-		$zigbee_path = realpath(dirname(__FILE__) . '/../../resources/zigbeed');
-		$cmd = '/usr/bin/python3 ' . $rfxcom_path . '/zigbeed.py';
-		$cmd .= ' --device ' . $port;
-		$cmd .= ' --loglevel ' . log::convertLogLevel(log::getLogLevel('zigbee'));
-		$cmd .= ' --socketport ' . config::byKey('socketport', 'zigbee');
-		$cmd .= ' --callback ' . network::getNetworkAccess('internal', 'proto:127.0.0.1:port:comp') . '/plugins/rfxcom/core/php/jeeZigbee.php';
-		$cmd .= ' --apikey ' . jeedom::getApiKey('zigbee');
-		$cmd .= ' --cycle ' . config::byKey('cycle', 'zigbee');
-		$cmd .= ' --pid ' . jeedom::getTmpFolder('zigbee') . '/deamon.pid';
+    self::deamon_stop();
+    $deamon_info = self::deamon_info();
+    if ($deamon_info['launchable'] != 'ok') {
+      throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
+    }
+    $port = config::byKey('port', 'zigbee');
+    if ($port != 'auto') {
+      $port = jeedom::getUsbMapping($port);
+    }
+    $zigbee_path = realpath(dirname(__FILE__) . '/../../resources/zigbeed');
+    $cmd = '/usr/bin/python3 ' . $rfxcom_path . '/zigbeed.py';
+    $cmd .= ' --device ' . $port;
+    $cmd .= ' --loglevel ' . log::convertLogLevel(log::getLogLevel('zigbee'));
+    $cmd .= ' --socketport ' . config::byKey('socketport', 'zigbee');
+    $cmd .= ' --callback ' . network::getNetworkAccess('internal', 'proto:127.0.0.1:port:comp') . '/plugins/rfxcom/core/php/jeeZigbee.php';
+    $cmd .= ' --apikey ' . jeedom::getApiKey('zigbee');
+    $cmd .= ' --cycle ' . config::byKey('cycle', 'zigbee');
+    $cmd .= ' --pid ' . jeedom::getTmpFolder('zigbee') . '/deamon.pid';
     $cmd .= ' --data_folder '. realpath(dirname(__FILE__) . '/../../data');
     $cmd .= ' --controller '. config::byKey('controller', 'zigbee');;
-		log::add('zigbee', 'info', 'Lancement démon zigbeed : ' . $cmd);
-		exec($cmd . ' >> ' . log::getPathToLog('zigbee') . ' 2>&1 &');
-		return true;
-	}
+    log::add('zigbee', 'info', 'Lancement démon zigbeed : ' . $cmd);
+    exec($cmd . ' >> ' . log::getPathToLog('zigbee') . ' 2>&1 &');
+    return true;
+  }
   
   public static function deamon_stop() {
-		$pid_file = jeedom::getTmpFolder('zigbee') . '/deamon.pid';
-		if (file_exists($pid_file)) {
-			$pid = intval(trim(file_get_contents($pid_file)));
-			system::kill($pid);
-		}
-		system::kill('zigbeed.py');
-		system::fuserk(config::byKey('socketport', 'zigbee'));
-		$port = config::byKey('port', 'zigbee');
-		if ($port != 'auto') {
-			system::fuserk(jeedom::getUsbMapping($port));
-		}
-		sleep(1);
-	}
+    $pid_file = jeedom::getTmpFolder('zigbee') . '/deamon.pid';
+    if (file_exists($pid_file)) {
+      $pid = intval(trim(file_get_contents($pid_file)));
+      system::kill($pid);
+    }
+    system::kill('zigbeed.py');
+    system::fuserk(config::byKey('socketport', 'zigbee'));
+    $port = config::byKey('port', 'zigbee');
+    if ($port != 'auto') {
+      system::fuserk(jeedom::getUsbMapping($port));
+    }
+    sleep(1);
+  }
   
-
-/*     * *********************Méthodes d'instance************************* */
-
-
-/*     * **********************Getteur Setteur*************************** */
+  
+  /*     * *********************Méthodes d'instance************************* */
+  
+  
+  /*     * **********************Getteur Setteur*************************** */
 }
 
 class zigbeeCmd extends cmd {
   /*     * *************************Attributs****************************** */
-
+  
   
   /*     * ***********************Methode static*************************** */
   
@@ -134,8 +128,8 @@ class zigbeeCmd extends cmd {
   /*     * *********************Methode d'instance************************* */
   
   public function execute($_options = array()) {
-
+    
   }
-
-/*     * **********************Getteur Setteur*************************** */
+  
+  /*     * **********************Getteur Setteur*************************** */
 }
--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,50 +1,76 @@
 <?php
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
-if (!isConnect()) {
-    include_file('desktop', '404', 'php');
-    die();
+if (!isConnect('admin')) {
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
 <form class="form-horizontal">
-    <fieldset>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 1}}</label>
-            <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param1" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
-            <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param2" value="80" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
-            <div class="col-lg-2">
-                <select class="configKey form-control" data-l1key="param3">
-                    <option value="value1">value1</option>
-                    <option value="value2">value2</option>
-                </select>
-            </div>
-        </div>
+  <fieldset>
+    <legend><i class="icon loisir-darth"></i> {{Démon}}</legend>
+    <div class="form-group">
+      <label class="col-sm-4 control-label">{{Type de controlleur}}</label>
+      <div class="col-sm-2">
+        <select class="configKey form-control" data-l1key="controller">
+          <option value="ezsp">{{EZSP}}</option>
+          <option value="deconz">{{Deconz}}</option>
+          <option value="zigate">{{Zigate}}</option>
+          <option value="cc">{{CC}}</option>
+          <option value="xbee">{{Xbee}}</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-4 control-label">{{Port Zigbee}}</label>
+      <div class="col-sm-2">
+        <select class="configKey form-control" data-l1key="port">
+          <option value="none">{{Aucun}}</option>
+          <option value="auto">{{Auto}}</option>
+          <?php
+          foreach (jeedom::getUsbMapping() as $name => $value) {
+            echo '<option value="' . $name . '">' . $name . ' (' . $value . ')</option>';
+          }
+          foreach (ls('/dev/', 'tty*') as $value) {
+            echo '<option value="/dev/' . $value . '">/dev/' . $value . '</option>';
+          }
+          ?>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-4 control-label">{{Port socket interne}}</label>
+      <div class="col-sm-2">
+        <input class="configKey form-control" data-l1key="socketport" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-4 control-label">{{Cycle (s)}}</label>
+      <div class="col-sm-2">
+        <input class="configKey form-control" data-l1key="cycle" />
+      </div>
+    </div>
   </fieldset>
 </form>
 
+<script>
+$('#bt_manageRfxComProtocole').on('click', function () {
+  $('#md_modal2').dialog({title: "{{Gestion des protocoles RFXCOM}}"});
+  $('#md_modal2').load('index.php?v=d&plugin=rfxcom&modal=manage.protocole').dialog('open');
+});
+</script>
