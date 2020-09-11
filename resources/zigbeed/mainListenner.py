4f73ac984e50bf630f9f33f1d87902bfe051b917
Loïc <loic@jeedom.com>
2020-09-11 16:00:32 +0200

    same

--- resources/zigbeed/mainListenner.py
+++ resources/zigbeed/mainListenner.py
@@ -61,10 +61,7 @@ class MainListener:
 	def cluster_command(self, cluster, command_id, *args):
 		device = cluster.endpoint.device
 		logging.info("****************** cluster_command - Cluster: %s ClusterId: 0x%04x command_id: %s args: %s" %(cluster, cluster.cluster_id, command_id, args))
-		infos = {
-			"value" : str(args[0]),
-			"cluster_name" : cluster.name,
-		}
+		infos = {"value" : str(args[0]),"cluster_name" : cluster.name}
 		shared.JEEDOM_COM.add_changes('devices::'+str(cluster.endpoint.device._ieee)+'::'+str(cluster.endpoint._endpoint_id)+'::'+str(cluster.cluster_id)+'::cmd',infos)
 
 
