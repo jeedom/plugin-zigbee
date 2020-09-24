2e58587fd015bedda46989062d1e77be10b8af45
Loïc <loic@jeedom.com>
2020-09-24 15:34:11 +0200

    fix

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y python3 python3-pip
 echo 60 > ${PROGRESS_FILE}
 sudo pip3 install zigpy
 echo 65 > ${PROGRESS_FILE}
-sudo pip3 install bellows>=0.20.1
+sudo pip3 install bellows==0.20.1
 echo 70 > ${PROGRESS_FILE}
 sudo pip3 install zha-quirks
 echo 75 > ${PROGRESS_FILE}
@@ -21,7 +21,7 @@ sudo pip3 install zigpy_znp
 echo 80 > ${PROGRESS_FILE}
 sudo pip3 install zigpy-xbee
 echo 85 > ${PROGRESS_FILE}
-sudo pip3 install zigpy-deconz
+sudo pip3 install zigpy-deconz==0.10
 echo 90 > ${PROGRESS_FILE}
 sudo pip3 install zigpy-zigate
 echo 95 > ${PROGRESS_FILE}
