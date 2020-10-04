e0ba1527b5f8a6b392d2b1b61353ac8c7f47a89c
Loïc <loic@jeedom.com>
2020-10-04 18:06:43 +0200

    bugfix

--- desktop/modal/network.php
+++ desktop/modal/network.php
@@ -162,8 +162,8 @@ if (!isConnect('admin')) {
           tr += '<td>';
           tr += data[i].status;
           tr += '<td>';
-          tr += '<a class="btn btn-default btn-xs bt_refreshZigbeeDeviceInfo"><i class="fa fa-info"></i> {{Info}}</a> ';
-          tr += '<a class="btn btn-success btn-xs bt_initializeZigbeeDevice"><i class="fas fa-sync"></i> {{Rafraichir informations}}</a> ';
+          tr += '<a class="btn btn-default btn-xs bt_infoZigbeeDevice"><i class="fa fa-info"></i> {{Info}}</a> ';
+          tr += '<a class="btn btn-success btn-xs bt_refreshZigbeeDeviceInfo"><i class="fas fa-sync"></i> {{Rafraichir informations}}</a> ';
           tr += '<a class="btn btn-warning btn-xs bt_initializeZigbeeDevice"><i class="fas fa-sync"></i> {{Réinitialiser}}</a> ';
           tr += '<a class="btn btn-danger btn-xs bt_removeZigbeeDevice"><i class="fa fa-trash"></i> {{Supprimer}}</a> ';
           tr += '</td>';
