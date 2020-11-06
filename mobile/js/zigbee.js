4db7ff233426b31529468472e5ee7733118eb4f7
Loïc <loic@jeedom.com>
2020-11-06 09:57:16 +0100

    add mobile ui

---
+++ mobile/js/zigbee.js
@@ -0,0 +1,66 @@
+/* This file is part of Plugin openzwave for jeedom.
+*
+* Plugin openzwave for jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Plugin openzwave for jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Plugin openzwave for jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function initZigbeeZigbee() {
+  $('body').off('zigbee::includeDevice').on('zigbee::includeDevice', function (_event, _options) {
+    $('.eqLogicAttr[data-l1key=id]').value('');
+    if (_options != '') {
+      $("#div_configIncludeDevice").show();
+      $('.eqLogicAttr[data-l1key=id]').value(_options);
+    }
+  });
+  
+  $('.changeIncludeState').off('click').on('click', function () {
+    let instance = $(this).attr('data-instance');
+    jeedom.zigbee.application.include({
+      instance:instance,
+      duration : 180,
+      error: function (error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      },
+      success: function () {
+        $('#div_inclusionAlert').show()
+        $('#div_inclusionAlert').html('{{Mode inclusion actif pendant 3 minutes pour le démon}} '+instance);
+        setTimeout(function(){ $('#div_inclusionAlert').hideAlert() }, 3*60000);
+      }
+    });
+  });
+  
+  $('#bt_validateConfigDevice').on('click', function() {
+    jeedom.eqLogic.save({
+      type: 'zigbee',
+      eqLogics: $("#div_configIncludeDevice").getValues('.eqLogicAttr'),
+      error: function(error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+        $('.eqLogicAttr[data-l1key=id]').value('');
+      },
+      success: function() {
+        $("#div_configIncludeDevice").hide();
+      }
+    });
+  });
+  
+  jeedom.object.all({
+    success: function(objects) {
+      var options = '';
+      for (var i in objects) {
+        options += '<option value="' + objects[i].id + '">' + objects[i].name + '</option>'
+      }
+      $('.eqLogicAttr[data-l1key=object_id]').html(options);
+      $('.eqLogicAttr[data-l1key=object_id]').selectmenu("refresh");
+    }
+  });
+}
