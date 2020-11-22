26c14aaa56e43bd063bff7a1112c017123aceab9
loic <loicjeedom.com>
2020-11-22 12:22:36 +0100

    improve code

--- resources/zigbeed/restServer.py
+++ resources/zigbeed/restServer.py
@@ -174,7 +174,7 @@ class DeviceHandler(RequestHandler):
 				if device == None :
 					if not 'type' in self.json_args or self.json_args['type'] == '' :
 						self.json_args['type'] = None
-					device = shared.ZIGPY.devices[shared.ZIGPY].endpoints[zigpy.zcl.clusters.general.GreenPowerProxy.endpoint_id].out_clusters[zigpy.zcl.clusters.general.GreenPowerProxy.cluster_id].create_device(utils.convertStrToIEU64(self.json_args['ieee']),self.json_args['type'])
+					device = shared.ZIGPY.get_device(nwk=0).endpoints[zigpy.zcl.clusters.general.GreenPowerProxy.endpoint_id].out_clusters[zigpy.zcl.clusters.general.GreenPowerProxy.cluster_id].create_device(utils.convertStrToIEU64(self.json_args['ieee']),self.json_args['type'])
 				if self.json_args['key'] == '':
 					device.endpoints[zigpy.zcl.clusters.general.GreenPowerProxy.endpoint_id].in_clusters[zigpy.zcl.clusters.general.GreenPowerProxy.cluster_id].setKey(None)
 				else:
