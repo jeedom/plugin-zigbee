8109184102ed3b7a0257de4249947be622d9c0f9
loic <loic@jeedom.com>
2021-03-04 13:14:59 +0100

    prevent wrong battery

--- core/php/jeeZigbee.php
+++ core/php/jeeZigbee.php
@@ -152,7 +152,7 @@ if (isset($result['devices'])) {
 							if( $attribut_id == 33){
 								$zigbee->batteryStatus($value['value']);
 							}
-							if( $attribut_id == 32){
+							if( $attribut_id == 32 && $value['value'] > 0){
 								if(is_array($zigbee->getConfiguration('maxBatteryVoltage',0)) || $value > $zigbee->getConfiguration('maxBatteryVoltage',0)){
 									$zigbee->setConfiguration('maxBatteryVoltage',$value['value']);
 									$zigbee->save();
