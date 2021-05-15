57ad24b481a91d0fd15d2292cb355e487a0f25d2
loic <loic@jeedom.com>
2021-05-15 16:17:18 +0200

    change default time

--- resources/zigbeed/channels/general.py
+++ resources/zigbeed/channels/general.py
@@ -197,7 +197,7 @@ class PollControl():
 	"""Poll Control channel."""
 	CHECKIN_INTERVAL = 60 * 60 * 4  # 60min
 	CHECKIN_FAST_POLL_TIMEOUT = 10 * 4  # 10s
-	LONG_POLL = 5 * 4  # 5s
+	LONG_POLL = 8 * 60 * 4  # 8min
 
 	async def initialize(cluster):
 		 try:
