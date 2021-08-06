7f5fd5408d091de1bcf6160fd0cef95b96aa23d2
Loïc <loic@jeedom.com>
2021-08-06 09:29:24 +0200

    bugfix

--- desktop/modal/group.php
+++ desktop/modal/group.php
@@ -18,28 +18,28 @@ if (!isConnect('admin')) {
   throw new Exception('401 Unauthorized');
 }
 $eqLogic = zigbee::byId(init('id'));
-if(!is_object($eqLogic)){
-  throw new \Exception(__('Equipement introuvable : ',__FILE__).init('id'));
+if (!is_object($eqLogic)) {
+  throw new \Exception(__('Equipement introuvable : ', __FILE__) . init('id'));
 }
-sendVarToJS('zigbeeNodeInstance',$eqLogic->getConfiguration('instance',1));
-$group_data = zigbee::request($eqLogic->getConfiguration('instance',1),'/group/info',array('id'=>explode('|',$eqLogic->getLogicalId())[1]));
-sendVarToJS('group_id',explode('|',$eqLogic->getLogicalId())[1]);
-$groupableEqlogics = zigbee::getGroupable($eqLogic->getConfiguration('instance',1));
+sendVarToJS('zigbeeNodeInstance', $eqLogic->getConfiguration('instance', 1));
+$group_data = zigbee::request($eqLogic->getConfiguration('instance', 1), '/group/info', array('id' => explode('|', $eqLogic->getLogicalId())[2]));
+sendVarToJS('group_id', explode('|', $eqLogic->getLogicalId())[1]);
+$groupableEqlogics = zigbee::getGroupable($eqLogic->getConfiguration('instance', 1));
 $devices = array();
 $eqLogics = eqLogic::byType('zigbee');
 foreach ($eqLogics as $device) {
-  if ($device->getConfiguration('isgroup',0) == 0 && $device->getConfiguration('ischild',0) == 0){
-    $eqLogicArray =array();
+  if ($device->getConfiguration('isgroup', 0) == 0 && $device->getConfiguration('ischild', 0) == 0) {
+    $eqLogicArray = array();
     $eqLogicArray['HumanNameFull'] = $device->getHumanName(true);
     $eqLogicArray['HumanName'] = $device->getHumanName();
     $eqLogicArray['id'] = $device->getId();
-    $eqLogicArray['img'] = 'plugins/zigbee/core/config/devices/'.zigbee::getImgFilePath($device->getConfiguration('device'));
+    $eqLogicArray['img'] = 'plugins/zigbee/core/config/devices/' . zigbee::getImgFilePath($device->getConfiguration('device'));
     $devices[$device->getLogicalId()] = $eqLogicArray;
   }
 }
 $listAddGroup = array();
 foreach ($groupableEqlogics as $groupable) {
-  if (!in_array($groupable['ieee'],$group_data['members'])){
+  if (!in_array($groupable['ieee'], $group_data['members'])) {
     $listAddGroup[] = $groupable;
   }
 }
@@ -89,15 +89,15 @@ sendVarToJS('listAddGroup', $listAddGroup);
           <?php
           $tr = '';
           foreach ($group_data['members'] as $member) {
-            $img ='';
-            $fullname='';
-            if (isset($devices[$member])){
+            $img = '';
+            $fullname = '';
+            if (isset($devices[$member])) {
               $img = $devices[$member]['img'];
               $fullname = $devices[$member]['HumanNameFull'];
             }
-            $tr .= '<tr data-ieee="'.$member.'">';
+            $tr .= '<tr data-ieee="' . $member . '">';
             $tr .= '<td style="font-size:0.8em !important;">';
-            $tr .= '<img class="lazy" src="'.$img.'" height="40" width="40" />';
+            $tr .= '<img class="lazy" src="' . $img . '" height="40" width="40" />';
             $tr .= '</td>';
             $tr .= '<td style="font-size:0.8em !important;">';
             $tr .= $member;
@@ -113,65 +113,83 @@ sendVarToJS('listAddGroup', $listAddGroup);
           ?>
         </tbody>
       </table>
-      
+
     </div>
   </fieldset>
 </form>
 
 <script>
-$('.bt_addToZigbeeGroup').off('click').on('click',function(){
-  var inputOptions = [];
-  for(var i in listAddGroup){
-    inputOptions.push({value : listAddGroup[i].ieee,text : listAddGroup[i].name});
-  }
-  if(inputOptions.length == 0){
-    $('#div_groupeZigbeeAlert').showAlert({message: '{{Aucun module ne peut etre ajouté dans ce groupe}} ', level: 'warning'});
-    return;
-  }
-  bootbox.prompt({
-    title: "{{Quel module voulez vous ajouter au groupe ?}}",
-    value : inputOptions[0].value,
-    inputType: 'select',
-    inputOptions:inputOptions,
-    callback: function (device_result) {
-      if(device_result === null){
-        return;
+  $('.bt_addToZigbeeGroup').off('click').on('click', function() {
+    var inputOptions = [];
+    for (var i in listAddGroup) {
+      inputOptions.push({
+        value: listAddGroup[i].ieee,
+        text: listAddGroup[i].name
+      });
+    }
+    if (inputOptions.length == 0) {
+      $('#div_groupeZigbeeAlert').showAlert({
+        message: '{{Aucun module ne peut etre ajouté dans ce groupe}} ',
+        level: 'warning'
+      });
+      return;
+    }
+    bootbox.prompt({
+      title: "{{Quel module voulez vous ajouter au groupe ?}}",
+      value: inputOptions[0].value,
+      inputType: 'select',
+      inputOptions: inputOptions,
+      callback: function(device_result) {
+        if (device_result === null) {
+          return;
+        }
+        jeedom.zigbee.group.add_device({
+          instance: zigbeeNodeInstance,
+          ieee: device_result,
+          id: group_id,
+          error: function(error) {
+            $('#div_groupeZigbeeAlert').showAlert({
+              message: error.message,
+              level: 'danger'
+            });
+          },
+          success: function() {
+            $('#div_groupeZigbeeAlert').showAlert({
+              message: '{{Module ajouté avec succès au groupe}} ',
+              level: 'success'
+            });
+            $('#md_modal').dialog('close');
+            $('#md_modal').dialog({
+              title: "{{Configuration du groupe}}"
+            }).load('index.php?v=d&plugin=zigbee&modal=group&id=' + $('.eqLogicAttr[data-l1key=id]').value()).dialog('open');
+          }
+        });
       }
-      jeedom.zigbee.group.add_device({
-        instance : zigbeeNodeInstance,
-        ieee:device_result,
-        id:group_id,
-        error: function (error) {
-          $('#div_groupeZigbeeAlert').showAlert({message: error.message, level: 'danger'});
-        },
-        success: function () {
-          $('#div_groupeZigbeeAlert').showAlert({message: '{{Module ajouté avec succès au groupe}} ', level: 'success'});
-          $('#md_modal').dialog('close');
-          $('#md_modal').dialog({title: "{{Configuration du groupe}}"}).load('index.php?v=d&plugin=zigbee&modal=group&id='+$('.eqLogicAttr[data-l1key=id]').value()).dialog('open');
-        }
-      });
-    }
-  });
-})
+    });
+  })
 
-$('.bt_removeFromZigbeeGroup').off('click').on('click',function(){
-  var tr = $(this).closest('tr');
-  bootbox.confirm("{{Etês vous sur de vouloir supprimer ce noeud du groupe ?}}", function(result){
-    if(result){
-      jeedom.zigbee.group.delete_device({
-        instance : zigbeeNodeInstance,
-        ieee : tr.attr('data-ieee'),
-        id:group_id,
-        error: function (error) {
-          $('#div_groupeZigbeeAlert').showAlert({message: error.message, level: 'danger'});
-        },
-        success: function (data) {
-          $('#md_modal').dialog('close');
-          $('#md_modal').dialog({title: "{{Configuration du groupe}}"}).load('index.php?v=d&plugin=zigbee&modal=group&id='+$('.eqLogicAttr[data-l1key=id]').value()).dialog('open');
-        }
-      });
-    }
-  });
-})
-
+  $('.bt_removeFromZigbeeGroup').off('click').on('click', function() {
+    var tr = $(this).closest('tr');
+    bootbox.confirm("{{Etês vous sur de vouloir supprimer ce noeud du groupe ?}}", function(result) {
+      if (result) {
+        jeedom.zigbee.group.delete_device({
+          instance: zigbeeNodeInstance,
+          ieee: tr.attr('data-ieee'),
+          id: group_id,
+          error: function(error) {
+            $('#div_groupeZigbeeAlert').showAlert({
+              message: error.message,
+              level: 'danger'
+            });
+          },
+          success: function(data) {
+            $('#md_modal').dialog('close');
+            $('#md_modal').dialog({
+              title: "{{Configuration du groupe}}"
+            }).load('index.php?v=d&plugin=zigbee&modal=group&id=' + $('.eqLogicAttr[data-l1key=id]').value()).dialog('open');
+          }
+        });
+      }
+    });
+  })
 </script>
